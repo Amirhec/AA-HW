ac01f3bf4e167d357c07912622844a53e5f670d8
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-01-29 23:49:57 -0500

    homework

---
+++ w4d1 /w4d1.rb
@@ -0,0 +1,22 @@
+class GraphNode
+  attr_accessor :value, :neighbors
+
+  def initialize(value)
+    self.value = value
+    self.neighbors = []
+  end
+
+  def add_neighbor(node)
+    self.neighbors << node
+  end
+end
+
+def bfs(starting_node, target_value)
+  queue = [starting_node]
+  
+
+  end
+  
+
+end
+
